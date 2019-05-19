fa2f8cd38d2a3d2d1038ec31393e39b6aab9551e
Shalisska <alena.simonova83@gmail.com>
2019-05-20 01:21:03 +0300

    new db

--- CrmUi/Catalog.Designer.cs
+++ CrmUi/Catalog.Designer.cs
@@ -29,25 +29,65 @@
         private void InitializeComponent()
         {
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Add = new System.Windows.Forms.Button();
+            this.Change = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView
             // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
             this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.MultiSelect = false;
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(666, 358);
             this.dataGridView.TabIndex = 0;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(161, 398);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Change
+            // 
+            this.Change.Location = new System.Drawing.Point(306, 398);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(75, 23);
+            this.Change.TabIndex = 2;
+            this.Change.Text = "Изменить";
+            this.Change.UseVisualStyleBackColor = true;
+            this.Change.Click += new System.EventHandler(this.Change_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(441, 398);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
             // 
             // Catalog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(690, 450);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.Add);
             this.Controls.Add(this.dataGridView);
             this.Name = "Catalog";
             this.Text = "Catalog";
@@ -60,5 +100,8 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Change;
+        private System.Windows.Forms.Button Delete;
     }
 }
--- CrmUi/Catalog.cs
+++ CrmUi/Catalog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CrmBl.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,15 +15,44 @@ namespace CrmUi
     public partial class Catalog<T> : Form
         where T : class
     {
-        public Catalog(DbSet<T> set)
+        CrmContext db;
+
+        public Catalog(DbSet<T> set, CrmContext _db)
         {
             InitializeComponent();
+            db = _db;
+            var res = db.Set(typeof(T)).ToListAsync();
+            var res1 = res.Result;
             dataGridView.DataSource = set.Local.ToBindingList();
         }
 
         private void Catalog_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (typeof(T) == typeof(Product))
+            {
+
+            }
+            else if (typeof(T) == typeof(Seller))
+            {
+            }
+            else if(typeof(T) == typeof(Customer))
+            {
+
+            }
+            else if (typeof(T) == typeof(Check))
+            {
+
+            }
+        }
+
+        private void Change_Click(object sender, EventArgs e)
+        {
+            var id = dataGridView.SelectedRows[0].Cells[0].Value;
         }
     }
 }
